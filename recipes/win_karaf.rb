d13a477d0e6c428b5aee39506e70e5ffaa36f520
Karol Drazek <karol.drazek@cognifide.com>
2016-10-14 16:19:55 +0200

    Adding Karaf deployment as a service

--- recipes/win_karaf.rb
+++ recipes/win_karaf.rb
@@ -26,6 +26,35 @@ include_recipe 'java::default'
 # Install Chef Windows toolset
 include_recipe 'windows::default'
 
+# Install service wrapper
+directory 'c:\content\nssm' do
+  recursive true
+  action :create
+end
+
+remote_file 'c:\content\nssm\nssm-2.24.zip' do
+  source 'https://nssm.cc/release/nssm-2.24.zip'
+  action :create
+end
+
+windows_zipfile 'c:\content\nssm' do
+  source 'c:\content\nssm\nssm-2.24.zip'
+  action :unzip
+
+  not_if { ::File.exist?('c:\content\nssm\nssm-2.24\win64\nssm.exe') }
+end
+
+link 'c:\content\nssm\current' do
+  to 'c:\content\nssm\nssm-2.24'
+end
+
+env 'Path' do
+  value 'c:\\content\\nssm\\current\\win64\\'
+  delim ';'
+  action :modify
+end
+
+# Karaf
 directory 'c:\content\karaf' do
   action :create
 end
@@ -44,4 +73,36 @@ end
 
 link 'c:\content\karaf\current' do
   to 'c:\content\karaf\apache-karaf-2.3.9'
+end
+
+execute 'config-karaf-service' do
+  # NSSM creates service in Automatic state,
+  # so enable action is not requried in service
+  command 'nssm install karaf c:\content\karaf\current\bin\karaf.bat'
+  action :run
+
+  not_if { ::Win32::Service.exists?('karaf') }
+end
+
+service 'karaf' do
+  action :start
+end
+
+karaf_service_config =
+  'HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Services\karaf\Parameters'
+
+registry_key karaf_service_config do
+  values [{
+    name: 'AppEnvironmentExtra',
+    type: :multi_string,
+    data: [
+      "JAVA_MIN_MEM=#{node['aet']['karaf']['java_min_mem']}",
+      "JAVA_MAX_MEM=#{node['aet']['karaf']['java_max_mem']}",
+      "JAVA_PERM_MEM=#{node['aet']['karaf']['java_min_perm_mem']}",
+      "JAVA_MAX_PERM_MEM=#{node['aet']['karaf']['java_max_perm_mem']}"
+    ]
+  }]
+  action :create
+
+  notifies :restart, 'service[karaf]', :delayed
 end
