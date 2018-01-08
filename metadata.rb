688f21516e7e57129782f7d440b856f26029b501
Wiiitek <kubiczak.jakub@gmail.com>
2018-01-08 23:51:15 +0100

    version upgrade

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email  'karol.drazek@cognifide.com'
 license           'Apache 2.0'
 description       'Installs/Configures aet'
 long_description  'Installs/Configures aet'
-version           '1.4.16-SNAPSHOT'
+version           '2.0.0-SNAPSHOT'
 
 depends           'apache2', '~> 3.2.2'
 depends           'java', '~> 1.13'
