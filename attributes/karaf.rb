c5ff1bcafcb71cde04fb5eb7401b932e88ace43a
Wiiitek <wiiitek@users.noreply.github.com>
2018-06-04 15:37:46 +0200

    Karaf upgrade (#27)
    * Karaf upgrade
    
    * docs update

--- attributes/karaf.rb
+++ attributes/karaf.rb
@@ -22,7 +22,7 @@
 # KARAF
 
 default['aet']['karaf']['source'] =
-  'https://archive.apache.org/dist/karaf/4.1.4/apache-karaf-4.1.4.tar.gz'
+  'https://archive.apache.org/dist/karaf/4.1.5/apache-karaf-4.1.5.tar.gz'
 
 default['aet']['karaf']['user'] = 'karaf'
 default['aet']['karaf']['group'] = 'karaf'
