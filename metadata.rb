5a59e55d68a768b67c0e4f3d3aff275a355ce06c
Wiiitek <kubiczak.jakub@gmail.com>
2018-08-02 21:54:55 +0200

    downgrading apache to pass tests

--- metadata.rb
+++ metadata.rb
@@ -6,7 +6,7 @@ description       'Installs/Configures aet'
 long_description  'Installs/Configures aet'
 version           '5.0.0'
 
-depends           'apache2', '~> 5.0.1'
+depends           'apache2', '~> 3.3.1'
 depends           'java', '~> 2.2.0'
 depends           'sc-mongodb', '~> 1.2.0'
 
