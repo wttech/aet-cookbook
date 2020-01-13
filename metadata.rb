1c00022d409a97723fb7bbe237aa8ba27b2f636e
Marcin Walkowski <marcin.walkowski@cognifide.com>
2020-01-13 11:39:48 +0100

    Bumping apache2 cookbook version from 3.3.1 to 5.0.1

--- metadata.rb
+++ metadata.rb
@@ -4,9 +4,9 @@ maintainer_email  'karol.drazek@cognifide.com'
 license           'Apache 2.0'
 description       'Installs/Configures aet'
 long_description  'Installs/Configures aet'
-version           '5.1.2'
+version           '5.1.3'
 
-depends           'apache2', '~> 3.3.1'
+depends           'apache2', '~> 5.0.1'
 depends           'java', '~> 2.2.0'
 depends           'sc-mongodb', '~> 1.2.0'
 
